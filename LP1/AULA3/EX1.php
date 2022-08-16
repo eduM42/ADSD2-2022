7cb2f9f28f0ddf62a5a81edd7f4bd9f04fb2492f
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-08-16 20:05:38 -0300

    a

--- LP1/AULA3/EX1.php
+++ LP1/AULA3/EX1.php
@@ -4,8 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EX1 - AULA 3</title>
+    <style>
+    </style>
 </head>
-<body>
+<body style="backgroud-color: black; color: white">
     <?php
         $carros = array("Onix", "Jeep", "Corolla", "Civic", "Vectra");
         $cores = array();
