f4c0f3e4b02759dc44775035722e15b704f281b6
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-08-26 21:09:34 -0300

    md

--- ED1/AULA4/ex2.c
+++ ED1/AULA4/ex2.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-    int v1 = 7; // Declara a primeira vairável
-    int v2 = 12; // Declara a segunda variável
-
+    int v1 = 0, v2 = 0; // Declara a primeira vairável
+    printf("Insira o valor da primeira variável: ");
+    scanf("%d", &v1);
+    printf("Insira o valor da segunda variável: ");
+    scanf("%d", &v2);
     if(&v1 > &v2){ // Se o valor do ENDEREÇO de v1 for maior que v2
         printf("\n\nO endereço da variável v1 é maior que a de v2: %x\nValor de v1: %d\n\n", &v1, v1); // Imprime a mensagem e o endereço de v1
     }else if(&v2 > &v1){ // Se o valor do ENDEREÇO de v2 for maior que v1
