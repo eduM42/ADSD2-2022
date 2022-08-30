7370626a3e7511b7bbb998006cc5da6e7db8b7cb
EduOzorio <eduardo.ozorio@etec.sp.gov.br>
2022-08-30 20:25:14 -0300

    modified

--- LP1/AULA5/EXEMPLOS/EX1.php
+++ LP1/AULA5/EXEMPLOS/EX1.php
@@ -29,4 +29,28 @@
         echo "$cont ";
         $cont -= 2;
     }while($cont >= 0);
+
+    echo "<h2>Usando o for</h2>";
+    echo "<p>Exibir números de 0 a 12</p>";
+    for($cont = 0; $cont <= 12; $cont++){
+        echo "$cont ";
+    }
+
+    echo "<p>Contagem regressiva de 12 até 0, contando de 2 em 2</p>";
+    for($cont = 12; $cont >= 0; $cont-=2){
+        echo "$cont ";
+    }
+
+    echo "<h2>Usando foreach</h2>";
+    echo "<p>Array indexado</p>";
+    $carros = array("Corolla", "Versa", "Jeep", "Ônix");
+    foreach($carros as $car){
+        echo "$car<br>";
+    }
+    echo "<p>Array associativo</p>";
+    $frutas = array("fruta1" => "mamão", "fruta2" => "abacaxi", "fruta3" => "morango",
+    "fruta4" => "laranja");
+    foreach($frutas as $ft => $fruta){
+        echo "$ft - $fruta<br>";
+    }
 ?>
