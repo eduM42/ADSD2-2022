d330d513bae2990fe0d14fa68d4a70d800ffa6a4
EduOzorio <eduardo.ozorio@etec.sp.gov.br>
2022-08-30 19:23:27 -0300

    mod

---
+++ LP1/AULA2/EX1.php
@@ -0,0 +1,12 @@
+<?php
+    $nome = "Eduardo Mira Ozório";
+    $turma = "ADSD2 - Turma A";
+    $email = "eduardo.ozorio@aluno.ifsp.edu.br";
+    $idade = "18";
+    
+    echo "Nome: $nome";
+    echo "<br>Turma: $turma";
+    echo "<br>Email: $email";
+    echo "<br>Idade: $idade";
+
+?>
---
+++ LP1/AULA2/EX2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+</head>
+<body>
+    <?php
+        // Programa para cálculo da média
+        $n1 = 5.5;
+        $n2 = 7.0;
+        $n3 = 4.0;
+        $m = ($n1 + $n2 + $n3)/3;
+        
+        echo "A média é: $m";
+    ?>
+</body>
+</html>
