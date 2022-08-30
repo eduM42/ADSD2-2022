b42d98ec0dc8f655eedf213521064aa1c201f4e0
EduOzorio <eduardo.ozorio@etec.sp.gov.br>
2022-08-30 20:02:42 -0300

    mod

--- LP1/AULA5/EXEMPLOS/EX1.php
+++ LP1/AULA5/EXEMPLOS/EX1.php
@@ -1,4 +1,6 @@
 <?php
+    echo "<h1>Estruturas de repetição</h1>";
+    echo "<h2>Usando while</h2>";
     echo "<p>Exibir números de 0 a 12</p>";
     $cont = 0;
     while($cont <= 12){
@@ -12,4 +14,19 @@
         echo "$cont ";
         $cont -= 2;
     }
+
+    echo "<h2>Usando do... while</h2>";
+    echo "<p>Exibir números de 0 a 12</p>";
+    $cont = 0;
+    do{
+        echo "$cont ";
+        $cont++;
+    }while($cont <= 12);
+
+    echo "<p>Contagem regressiva de 12 até 0, contando de 2 em 2</p>";
+    $cont = 12;
+    do{
+        echo "$cont ";
+        $cont -= 2;
+    }while($cont >= 0);
 ?>
