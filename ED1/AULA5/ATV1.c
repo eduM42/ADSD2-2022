fc53ef3d215e3fd6e4a646428a8cd2322235a46b
EduOzorio <eduardo.ozorio@etec.sp.gov.br>
2022-09-02 20:29:08 -0300

    correctex1

--- ED1/AULA5/ATV1.c
+++ ED1/AULA5/ATV1.c
@@ -22,20 +22,44 @@ int main(){
     }
     fclose(f);
 
-
     i = 0;
+    f = fopen("arq2.txt", "w");
     if(f == NULL){
         printf("Erro na abertura!!!\n");
         exit(1);
     }
-    f = fopen("arq2.txt", "w");
     while(entrada[i] != '\0'){
         entrada[i] = toupper(entrada[i]);
         fputc(entrada[i], f);
         i++;
     }
-
+    fclose(f);
 
     printf("\n\nArquivo escrito com sucesso!!\n\n");
+
+    FILE *fr;
+    fr = fopen("arq1.txt", "r");
+    char saida;
+
+    printf("\nEm letras minúsculas: ");
+    while(saida != EOF){
+        saida = fgetc(fr);
+        printf("%c", saida);
+    }
+    fclose(fr);
+
+
+    FILE *fr2;
+    fr2 = fopen("arq2.txt", "r");
+    char saida2;
+
+    printf("\n\nEm letras maiúsculas: ");
+    while(saida2 != EOF){
+        saida2 = fgetc(fr);
+        printf("%c", saida2);
+    }
+    fclose(fr2);
+
+    printf("\n\n");
     return 0;
 }
