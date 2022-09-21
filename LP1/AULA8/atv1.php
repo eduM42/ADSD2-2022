2e4baa309aa46ea5bec37988baa679ecb2cc4c44
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-09-21 18:18:15 -0300

    Update atv1.php

--- LP1/AULA8/atv1.php
+++ LP1/AULA8/atv1.php
@@ -1,23 +1,42 @@
 <?php
     $numeros = array();
     for($i = 0; $i < 15; $i++){
-        $numeros[$i] = rand(0,20);    
-    }
-
-    foreach($numeros as $num){
-        echo "Número = $num <br>";
-        $soma += $num;
-        
-    }
-    echo "<br>";
-    sort($numeros);
-    foreach($numeros as $ord){
-        echo "Ordenado = $ord <br>";
+        $numeros[$i] = rand(0,20);
+        echo "Posição $i = ".$numeros[$i]."<br>";
     }
     
-    $mediana = $numeros[7];
-    $media = $soma/15;
+    echo "<br>";
+    
 
-    echo "<br>Media = $media<br>";
-    echo "<br>Mediana = $mediana<br>";
+    echo "<br>Media = ".media($numeros)."<br>";
+    echo "<br>Mediana = ".mediana($numeros)."<br>";
+    echo "<br>Valor mais frequente =".moda($numeros);
+
+
+
+    function media($vetor){
+        $soma = 0;
+        foreach($vetor as $num){
+            $soma += $num;
+        }
+        $media = $soma/15;
+        return $media;
+    }
+
+    function mediana($vetor){
+        sort($vetor);
+        $mediana = $vetor[7];
+        return $mediana;
+    }
+
+    function moda ($v){
+        sort($v);
+        $contaValoresRepetidos = array_count_values($v);
+        $Valormax = max($contaValoresRepetidos);
+        foreach ($contaValoresRepetidos as $moda => $repeteValor){
+            if ($repeteValor == $Valormax){
+                return $moda;
+            }
+        }
+    }
 ?>
