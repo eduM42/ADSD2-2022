d02328991f020851651ce60735109d1e80c2aef8
eduM42 <107020621+eduM42@users.noreply.github.com>
2022-10-07 23:07:52 -0300

    a

--- LP1/AULA9/ATIVIDADES/atv2_a9.php
+++ LP1/AULA9/ATIVIDADES/atv2_a9.php
@@ -1,13 +1,9 @@
 <?php
+require 'func_conta.php';
     $numeros = array();
 
     for($i = 0; $i < 100; $i++){
         $numeros[$i] = rand(1,6);
     }
-
-    $ocorrencias = array_count_values($numeros);
-
-    for ($i=1; $i <= 6; $i++) { 
-        echo "O número $i possui a frequência de ".$ocorrencias[$i]."<br>";
-    }
+    frequencia($numeros);
 ?>
